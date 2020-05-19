1083f3f895d7736234ff3c5e9737c86840f8caa9
Randall Gray <randall.gray7260@gmail.com>
2020-05-19 10:01:52 -0500

    Finished setup instructions.

--- SuperHeroes2/Data/ApplicationDbContext.cs
+++ SuperHeroes2/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHeroes2.Models;
 
 namespace SuperHeroes2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
